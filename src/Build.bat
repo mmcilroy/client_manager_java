5d97f2d13c2444a4b841776b1bc88894c3d8d178
Mark McIlroy <m1rk.mci@gmail.com>
2015-04-01 14:17:22 +0100

    Delete Build.bat

--- src/Build.bat
+++
@@ -1,9 +0,0 @@
-set JAR="C:\Program Files\Java\jdk1.7.0_25\bin\jar"
-
-del *jar
-
-%JAR% -cf cmcommon.jar -C CmCommon\Bin .
-%JAR% -cf cmserver.jar -C CmServer\Bin .
-%JAR% -cf cmclient.jar -C CmClient\Bin .
-
-pause
